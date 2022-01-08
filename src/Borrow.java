0873f0f47e1fc78a589c49cf9d4576e60f604d62
phuocnov <phuocnguyenhuu806@gmail.com>
2022-01-08 17:26:11 +0700

    init

---
+++ src/Borrow.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.time.LocalDate;
+
+public class Borrow {
+    public Reader reader;
+    public Book book;
+    public LocalDate dateBorrow;
+    public LocalDate dateReturn;
+    public boolean isReturned;
+
+    Borrow(){
+        
+    }
+    Borrow(Book book, Reader reader, String dateBorrow, String dateReturn, boolean isReturned){
+        this.book = book;
+        this.reader = reader;
+        this.dateBorrow = LocalDate.parse(dateBorrow);
+        this.dateReturn = LocalDate.parse(dateReturn);
+        this.isReturned = isReturned;
+    }
+    public String print(){
+        String rs;
+        if(isReturned){
+            rs = "ID: "+ reader.ID + " Name: " + reader.name + "Book: " + book.name + " Date Borrow: " + dateBorrow.toString() + "Date Return: " + dateReturn.toString(); 
+        }
+        else{
+            rs = "ID: "+ reader.ID + " Name: " + reader.name + "Book: " + book.name + " Date Borrow: " + dateBorrow.toString();
+        }
+        return rs;
+    }
+}
---
+++ src/BorrowController.java
@@ -0,0 +1,8 @@
+package src;
+
+import java.util.ArrayList;
+
+public class BorrowController {
+    public ArrayList<Borrow> borrowData = new ArrayList<>();
+    
+}
